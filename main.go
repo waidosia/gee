a6e3ce16cff3d6b9a9791201268e4b9d6f9ec952
waidosia <57361888+waidosia@users.noreply.github.com>
2022-03-10 20:35:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 func main() {
 	r := gee.Default()
 	r.GET("/", func(c *gee.Context) {
-		c.String(http.StatusOK, "Hello Geektutu\n")
+		c.String(http.StatusOK, "Hello Go\n")
 	})
 	// index out of range for testing Recovery()
 	r.GET("/panic", func(c *gee.Context) {
-		names := []string{"geektutu"}
-		c.String(http.StatusOK, names[100])
+		names := []string{"geek"}
+		c.String(http.StatusOK, names[4])
 	})
 
 	r.Run(":9999")
