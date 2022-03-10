ab277e7d59d7789eefff7c9ba593d527110abe6e
eddy <2578052882@qq.com>
2022-03-10 20:52:50 +0800

    :art: 完善Demo

--- main.go
+++ main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"Gee/gee"
+	"log"
 	"net/http"
 )
+
+func middle() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		log.Println("中间件启动")
+		c.Next()
+	}
+}
 
 func main() {
 	r := gee.Default()
 	r.GET("/", func(c *gee.Context) {
-		c.String(http.StatusOK, "Hello Go\n")
+		c.String(http.StatusOK, "Hello Go")
 	})
-	// index out of range for testing Recovery()
-	r.GET("/panic", func(c *gee.Context) {
-		names := []string{"geek"}
-		c.String(http.StatusOK, names[4])
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"Username": c.PostForm("username"),
+		})
 	})
+	r.GET("/:name", func(c *gee.Context) {
+		c.String(http.StatusOK, "Hello %s", c.Param("name"), c.Path)
+	})
+	v1 := r.Group("/v1")
+	v1.Use(middle())
+	{
+		v1.GET("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "Hello v1")
+		})
+	}
 
-	r.Run(":9999")
+	r.Run(":8080")
 }
